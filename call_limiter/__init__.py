55e4c00ef77c94449c86e443dfa7038a1474018c
eyukselen <44461230+eyukselen@users.noreply.github.com>
2026-04-10 02:40:28 +0100

    automated versioning with setuptools_scm

--- call_limiter/__init__.py
+++ call_limiter/__init__.py
@@ -1,4 +1,10 @@
 from .limiter import CallLimiter, CallRetry, ResilientLimiter
 
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("call-limiter")
+except PackageNotFoundError:
+    __version__ = "0.0.0.dev0"
 
 __all__ = ['CallLimiter', 'CallRetry', 'ResilientLimiter']
